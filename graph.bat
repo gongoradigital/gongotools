e510593bc13ce3025a60c5975cc6f084267a764f
glorieux-f <frederic.glorieux@fictif.org>
2017-03-08 18:01:47 +0100

    devrait marcher

--- graph.bat
+++ graph.bat
@@ -4,10 +4,12 @@ set HERE=%~dp0
 :: set current directory if called by 2click
 cd /d %HERE%
 set SRCDIR=..\polemos\
-set DESTDIR=.\
+set DESTDIR=.\graph\
+if not exist %DESTDIR% mkdir %DESTDIR%
 echo Gongora, transformation : graphes 
 for /r %%F in (%SRCDIR%*.xml) do (
-  set FILENAME=%%~nF
-  echo %DESTDIR%%%~nF.xml
+  set DESTNAME=%%~nF
+  %HERE%xsltproc\xsltproc -o %DESTDIR%%%~nF-nodes.csv %HERE%polemos2nodes.xsl %%F
+  %HERE%xsltproc\xsltproc -o %DESTDIR%%%~nF-edges.csv %HERE%polemos2edges.xsl %%F
 )
 PAUSE
