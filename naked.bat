bed64075e54032542c881cfbfec7207012f9afcf
HectorRuizSoto <hector.ruiz.soto@gmail.com>
2017-09-25 13:39:41 +0200

    Nouvelle route en local

--- naked.bat
+++ naked.bat
@@ -4,7 +4,7 @@ set HERE=%~dp0
 :: set current directory if called by 2click
 cd /d %HERE%
 set SRCDIR=..\polemos\
-set DESTDIR=..\naked\
+set DESTDIR=..\polemos-naked\naked\
 echo Gongora, transformation : naked 
 for /r %%F in (%SRCDIR%*.xml) do (
   set FILENAME=%%~nF
